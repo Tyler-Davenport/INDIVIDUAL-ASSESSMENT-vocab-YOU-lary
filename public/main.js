614dadc731f702604b6ddb18b118560254fbe853
Tyler Davenport <tydavenport0415@gmail.com>
2024-12-02 21:30:45 -0600

    styled and fixed login and logout

--- public/main.js
+++ public/main.js
@@ -3,10 +3,6 @@ import 'bootstrap';
 import '../styles/main.scss';
 
 const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>WELCOME TO VOCAB-YOU-LARY!  🗄️</h1>
-  `;
-
   ViewDirectorBasedOnUserAuthStatus();
 };
 
